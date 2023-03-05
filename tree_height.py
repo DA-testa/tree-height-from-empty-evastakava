d0583c84bf5a8ab1b06de8e135eec44561bbbc83
evastakava <125592045+evastakava@users.noreply.github.com>
2023-03-05 17:38:36 +0000

    tests

--- tree_height.py
+++ tree_height.py
@@ -7,43 +7,41 @@ import numpy
 
 def compute_height(n, parents):
 
-    heights = [0] * n
-    for i in range(n):
-        if heights[i] == 0:
-            height = 1
-            parent = parents[i]
-            while parent != -1:
-                if heights[parent] == 0:
-                    height += 1
-                    parent = parents[parent]
-                else:
-                    height += heights[parent]
-                    break
-            heights[i] = height
+    tree = [[]for i in range (n)]
+    for i, parent in enumerate(parents):
+        if parent != -1:
+            tree[parent].append(i)
 
-    return max(heights)
+    root = numpy.where(parents == -1)[0][0]
+    q = [(root, 0)]
+    max_height = 0
 
+    while q:
+        node, height = q.pop(0)
+        max_height = max(max_height, height)
+        for child in tree[node]:
+            q.append((child, height + 1))
+    return max_height + 1
 
 def main():
 
-    source = input("Enter 'I' to input from keyboard or 'F' to input from file: ")
-    while source not in ['I', 'F']:
-        source = input("Invalid input.")
-    if source == "I":
+    input_type = input("Enter I to input from keyboard or F to input from file: ").upper()
+    if input_type[0] == "I":
         n = int(input())
-        parents = list(map(int, input().split()))
-    else:
+        parents1 = input()
+        parents = numpy.array(list(map(int, parents1.split())))
+        height = compute_height(n, parents)
+    elif input_type[0] == "F":
         filename = input("Enter file name: ")
-        while 'a' in filename not in filename:
-            filename = input("Invalid file name. Enter again: ")
-        try:
-            with open("input/" + filename, "r") as f:
-                n = int(f.readline().strip())
-                parents = list(map(int, f.readline().split()))
-        except FileNotFoundError:
-            print("File not found.")
+        if 'a' in filename:
+            print("Error. Try again.")
             return
-    height = compute_height(n, parents)   
+        with open(filename, 'r') as file:
+            n = int(file.readline())
+            parents1 = file.readline().strip()
+            parents = numpy.array(list(map(int, parents1.split())))
+            height = compute_height(n, parents)
+
     print(height)
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
