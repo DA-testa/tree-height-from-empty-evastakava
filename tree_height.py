67e03fdb0f47ae27d7a233125f0234e1ee42b535
evastakava <125592045+evastakava@users.noreply.github.com>
2023-03-05 18:09:32 +0000

    test

--- tree_height.py
+++ tree_height.py
@@ -7,18 +7,18 @@ import numpy
 
 def compute_height(n, parents):
 
-    tree = [[]for i in range (n)]
+    tree = [[]for i in range(n)]
 
     for i, parent in enumerate(parents):
         if parent != -1:
             tree[parent].append(i)
 
     root = numpy.where(parents == -1)[0][0]
-    q = [(root, 0)]
+    queue = [(root, 0)]
     max_height = 0
 
-    while q:
-        node, height = q.pop(0)
+    while queue:
+        node, height = queue.pop(0)
         max_height = max(max_height, height)
         for child in tree[node]:
             q.append((child, height + 1))
@@ -29,30 +29,32 @@ def main():
     input_type = input("Enter I to input from keyboard or F to input from file: ").upper()
     if input_type[0] == "I":
         n = int(input("Enter the number of nodes: "))
-        parents_input = input("Enter the parents array: ")
+        parents_input = input("Enter the parents list: ")
         parents = numpy.array(list(map(int, parents_input.split())))
+        height = compute_height(n, parents)
     elif input_type[0] == "F":
         filename = input("Enter file name: ")
         if 'a' in filename:
-            print("Error. File name contains the letter 'a. Please try again.")
+            print("Error. File name contains the letter 'a'. Please try again.")
             return
         try:
             with open(filename, 'r') as file:
                 n = int(file.readline())
                 parents_input = file.readline().strip()
                 parents = numpy.array(list(map(int, parents_input.split())))
-        except:
-            print("Error. Could not open file.")
+                height = compute_height(n, parents)
+        except FileNotFoundError:
+            print("Error.")
             return
     else:
-        print("Error: Invalid file name")
+        print("Invalid input type")
         return
-    
-    height = compute_height(n, parents)
-    
     print(height)
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
+
+
+if __name__ == '__main__':
+    sys.setrecursionlimit(10**7)  # max depth of recursion
+    threading.stack_size(2**27)   # new thread will get stack of such size
+    thread = threading.Thread(target=main)
+    thread.start()
+    # print(numpy.array([1,2,3]))
