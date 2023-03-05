1f0c72c6413766535811c2b84dcab929fb7742ca
evastakava <125592045+evastakava@users.noreply.github.com>
2023-03-05 18:12:33 +0000

    test

--- tree_height.py
+++ tree_height.py
@@ -1,60 +1,51 @@
-# python3
-
 import sys
 import threading
-import numpy
-
+import numpy as np
 
 def compute_height(n, parents):
-
-    tree = [[]for i in range(n)]
-
+    tree = [[] for i in range (n)]
     for i, parent in enumerate(parents):
-        if parent != -1:
+        if parent !=-1:
             tree[parent].append(i)
-
-    root = numpy.where(parents == -1)[0][0]
-    queue = [(root, 0)]
-    max_height = 0
+    root = np.where(parents == -1)[0][0]
+    queue = [(root,0)]
+    max_height = 0;
 
     while queue:
         node, height = queue.pop(0)
         max_height = max(max_height, height)
         for child in tree[node]:
-            queue.append((child, height + 1))
-    return max_height + 1
+            queue.append((child, height+1))
+    return max_height+1
 
 def main():
-
-    input_type = input("Enter I to input from keyboard or F to input from file: ").upper()
-    if input_type[0] == "I":
-        n = int(input("Enter the number of nodes: "))
-        parents_input = input("Enter the parents list: ")
-        parents = numpy.array(list(map(int, parents_input.split())))
+    text = input("Enter 'I' for input from keyboard or 'F' for input from file: ")
+    if text[0] == "I":
+        n = int(input("Enter number of nodes: "))
+        parents_str = input("Enter parent list separated by space: ")
+        parents = np.array(list(map(int, parents_str.split())))
         height = compute_height(n, parents)
-    elif input_type[0] == "F":
-        filename = input("Enter file name: ")
-        if 'a' in filename:
-            print("Error. File name contains the letter 'a'. Please try again.")
+    elif text[0] == "F":
+        file_name = input("Enter file name: ")
+        if "a" in file_name:
+            print("File name cannot contain letter 'a'.")
             return
         try:
-            with open(filename, 'r') as file:
+            with open("folder/" + file_name, 'r') as file:
                 n = int(file.readline())
-                parents_input = file.readline().strip()
-                parents = numpy.array(list(map(int, parents_input.split())))
+                parents_str = file.readline().strip()
+                parents = np.array(list(map(int, parents_str.split())))
                 height = compute_height(n, parents)
         except FileNotFoundError:
-            print("Error.")
+            print("File not found.")
             return
     else:
-        print("Invalid input type")
+        print("Invalid input option.")
         return
     print(height)
 
-
 if __name__ == '__main__':
-    sys.setrecursionlimit(10**7)  # max depth of recursion
-    threading.stack_size(2**27)   # new thread will get stack of such size
+    sys.setrecursionlimit(10**7)
+    threading.stack_size(2**27)
     thread = threading.Thread(target=main)
     thread.start()
-    # print(numpy.array([1,2,3]))
